b52d21aec37f6e6603d8787c7648b860e2633108
AlexStacker <lexusunx@gmail.com>
2018-08-17 23:59:57 +0800

    准备学习一下ts的Transformer

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-//
+/* eslint-disable indent */
 
 module.exports = {
   chainWebpack: config => {
@@ -11,14 +11,28 @@ module.exports = {
       .plugin('define')
         // 增加 环境变量 __DEV__
         .tap(args => {
-          let arg = args[0]
+          let arg = args[0];
           let rst = [
             {
               ...arg,
                 __DEV__: process.env.NODE_ENV !== 'production'
             }
-          ]
-          return rst
+          ];
+          return rst;
         })
+        .end()
+      .module
+        .rule('ts')
+          .use('ts-loader')
+          .tap(options => {
+            console.log(options);
+            options = {
+              ...options
+              // getCustomTransformers: () => ({
+              //   // before: [yourImportedTransformer]
+              // })
+            };
+            return options;
+          });
   }
-}
+};
