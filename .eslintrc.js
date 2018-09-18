47c262b4ccb9dbafb3fe993738c5bafcbd930e55
AlexStacker <lexusunx@gmail.com>
2018-09-18 16:34:42 +0800

    增加eslint验证

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,25 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  parser: 'typescript-eslint-parser',
+  extends: [
     'plugin:vue/essential',
     '@vue/standard',
     '@vue/typescript'
   ],
-  "plugins":[
-    "html"
+  plugins:[
+    "vue"
   ],
   rules: {
-    'semi': ["error", "always"],
+    'semi': ['error', 'always'],
+    'eqeqeq': ['error', 'smart'],
+    'space-before-function-paren': [2, 'never'], // 函数前不允许有空格
+    'no-unused-vars': [2, {
+      'vars': 'all',
+      'args': 'none'
+    }], // 不允许无用的变量
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
-  parserOptions: {
-    parser: 'typescript-eslint-parser'
-  }
+  parserOptions: {}
 }
